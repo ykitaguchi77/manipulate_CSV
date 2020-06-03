7d007e453fb1116f4d8baa13029cfe0e57893174
ykitaguchi77 <54803857+ykitaguchi77@users.noreply.github.com>
2020-06-04 01:11:36 +0900

    Add files via upload

---
+++ same_id.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import pandas as pd
+import csv
+from random import randint
+from time import sleep
+import math
+
+import glob
+import random
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import cv2
+
+'''
+#患者リスト
+list_path = 'C:\\Users\\ykita\\OneDrive\\デスクトップ\\手術リスト 201904_2.xlsx'
+
+#コルビスデータ
+data_path = 'C:\\Users\\ykita\\OneDrive\\デスクトップ\\exams200601.CSV'
+'''
